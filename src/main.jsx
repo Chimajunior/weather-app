c2c8cd24b7d15283a2fe2e482f3f09f772795066
chima <chimajunior14@gmail.com>
2025-01-20 16:00:21 +0000

    initial comm

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; // Import the main App component
+import "./index.css"; // Import global styles (or Tailwind setup)
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App /> {/* Render the main App component */}
+  </React.StrictMode>
+);

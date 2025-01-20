64377f9a33367866549c0f20b6f4d7d89f17125b
chima <chimajunior14@gmail.com>
2025-01-20 15:57:38 +0000

    footer init comm

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-blue-600 text-white text-center p-4">
+      <p>Powered by OpenWeatherMap</p>
+    </footer>
+  );
+}
+
+export default Footer;
